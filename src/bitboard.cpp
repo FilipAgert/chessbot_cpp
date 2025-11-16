5dfa8109e83acca1c13ac943b57411ff07ccb151
Filip Agert <filip.agert@gmail.com>
2025-11-16 20:40:44 +0100

    fix knight

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -6,18 +6,18 @@ namespace BitBoard{
         //Gets all possible attack squares a knight can make by shifting bitboard a few times.
         uint64_t out = (knight_loc & (~(top | row(6))) & (~right)) << (N+N+E); //NNE
         out |=  (knight_loc & (~(top)) & (~(right|col(6)))) << (N+E+E); //NEE
-        out |=  (knight_loc & (~(bottom)) & (~(right|col(6)))) << (N+E+E); //SEE
-        out |=  (knight_loc & (~(bottom | row(1))) & (~(right)) << (N+E+E)); //SSE
-        out |=  (knight_loc & (~(bottom | row(1))) & (~(left)) << (N+E+E)); //SSW
-        out |=  (knight_loc & (~(bottom)) & (~(left|col(1)))) << (N+E+E); //SWW
-        out |=  (knight_loc & (~(top)) & (~(left|col(1)))) << (N+E+E); //NWW
-        out |= (knight_loc & (~(top | row(6))) & (~left)) << (N+N+E); //NNW
+        out |=  (knight_loc & (~(bottom)) & (~(right|col(6)))) >> -(S+E+E); //SEE
+        out |=  (knight_loc & (~(bottom | row(1))) & (~(right)) >> -(S+S+E)); //SSE
+        out |=  (knight_loc & (~(bottom | row(1))) & (~(left)) >> -(S+S+W)); //SSW
+        out |=  (knight_loc & (~(bottom)) & (~(left|col(1)))) >> -(S+W+W); //SWW
+        out |=  (knight_loc & (~(top)) & (~(left|col(1)))) << (N+W+W); //NWW
+        out |= (knight_loc & (~(top | row(6))) & (~left)) << (N+N+W); //NNW
         return out;
     }
-    uint64_t ray(const uint64_t origin, const uint64_t dir){
+    uint64_t ray(const uint64_t origin, const uint8_t dir){
         //How to ensure no wrap-around?
         uint64_t hit = 0;
-        uint64_t mask =0;
+        uint64_t mask = edge_mask(dir);
         
         hit |= 
         shift_bb(origin, dir);
