09f40639a439ae741e629ea80006f48f67a926c6
Filip Agert <filip.agert@gmail.com>
2025-11-28 22:12:39 +0100

    format

--- include/constants.h
+++ include/constants.h
@@ -8,5 +8,7 @@ inline std::string const ID_author = "filipagert";
 inline std::string const ID_version = "0.1";
 
 inline uint8_t const err_val8 = 0b11111111;
-inline size_t const max_legal_moves = 267;//maximum amount of legal moves in one turn
+
+inline size_t const max_legal_moves = 267;
+// maximum amount of legal moves in one turn
 #endif
