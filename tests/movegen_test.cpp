7c92388283947b84a5cda51c45b3fd8658d903a6
Filip Agert <filip.agert@gmail.com>
2025-11-30 19:47:08 +0100

    add some testing positions

--- tests/movegen_test.cpp
+++ tests/movegen_test.cpp
@@ -19,6 +19,54 @@ using namespace dirs;
 using namespace masks;
 using namespace pieces;
 #define idx_from_string NotationInterface::idx_from_string
+
+TEST(Movegentest, kiwipete) {
+    std::vector<int> moves = {48, 2039, 97862, 4085603, 193690690};
+    std::string starting_fen =
+        "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 1 1";
+    int max = 3;
+    for (int i = 0; i < max; i++) {
+        int num_moves = movegen_benchmark::gen_num_moves(starting_fen, i + 1);
+        size_t expected = moves[i];
+        ASSERT_EQ(expected, num_moves);
+    }
+}
+TEST(Movegentest, p3) {
+    std::vector<int> moves = {14, 191, 2812, 43238};
+    std::string starting_fen = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1";
+    int max = 3;
+    for (int i = 0; i < max; i++) {
+        int num_moves = movegen_benchmark::gen_num_moves(starting_fen, i + 1);
+        size_t expected = moves[i];
+        ASSERT_EQ(expected, num_moves);
+    }
+}
+TEST(Movegentest, p4) {
+    std::vector<int> moves = {6, 264, 9467, 422333};
+    std::string starting_fen = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1";
+    int max = 3;
+    for (int i = 0; i < max; i++) {
+        int num_moves = movegen_benchmark::gen_num_moves(starting_fen, i + 1);
+        size_t expected = moves[i];
+        ASSERT_EQ(expected, num_moves);
+    }
+    starting_fen = "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1";
+    for (int i = 0; i < max; i++) {
+        int num_moves = movegen_benchmark::gen_num_moves(starting_fen, i + 1);
+        size_t expected = moves[i];
+        ASSERT_EQ(expected, num_moves);
+    }
+}
+TEST(Movegentest, p5) {
+    std::vector<int> moves = {44, 1486, 62379, 2103487};
+    std::string starting_fen = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
+    int max = 3;
+    for (int i = 0; i < max; i++) {
+        int num_moves = movegen_benchmark::gen_num_moves(starting_fen, i + 1);
+        size_t expected = moves[i];
+        ASSERT_EQ(expected, num_moves);
+    }
+}
 TEST(Movegentest, gen_moves) {
     BoardState state;
     std::string starting_fen = NotationInterface::starting_FEN();
