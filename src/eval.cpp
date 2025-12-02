ad83f013f1383803061cb4e44b8fea3fcf866c43
Filip Agert <filip.agert@gmail.com>
2025-12-02 20:57:35 +0100

    add lambda to sort by score

--- src/eval.cpp
+++ src/eval.cpp
@@ -19,20 +19,22 @@ int EvalState::eval(BoardState state) {
 void EvalState::partial_move_sort(std::array<Move, max_legal_moves> &moves,
                                   std::array<int, max_legal_moves> &scores, int num_moves,
                                   bool ascending) {
-    std::vector<std::pair<int, Move>> zipped;
+    std::vector<std::pair<Move, int>> zipped;
 
     for (size_t i = 0; i < num_moves; i++) {
         int s = scores[i];
         if (!ascending)
             s = s * (-1);
-        zipped.push_back({s, moves[i]});
+        zipped.push_back({moves[i], s});
     }
 
-    std::sort(zipped.begin(), zipped.end());
+    std::sort(zipped.begin(), zipped.end(), [](auto a, auto b) {
+        return a.second < b.second;
+    });  // Lambda to specify sort by second (score).
 
     for (size_t i = 0; i < num_moves; i++) {
-        moves[i] = zipped[i].second;
-        int s = zipped[i].first;
+        moves[i] = zipped[i].first;
+        int s = zipped[i].second;
         if (!ascending)
             s = s * (-1);
 
