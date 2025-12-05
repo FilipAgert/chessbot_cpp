e60e530929825486fe7cfd06a7e85725f20b0894
Filip Agert <filip.agert@gmail.com>
2025-12-06 00:21:38 +0100

    leading nowhere but started working on the reduce bitsets for magic bitboards

--- src/gen_magic_nums.cpp
+++ src/gen_magic_nums.cpp
@@ -1,4 +1,5 @@
 
+#include <algorithm>
 #include <bitboard.h>
 #include <cstdlib>
 #include <iostream>
@@ -31,9 +32,7 @@ uint64_t get_next_magic() {
  * @param[in] bits number of bits in the hash table
  * @return out key for bitboard
  */
-uint64_t get_key(uint64_t occ, uint64_t magic, uint8_t bits) {
-    return (occ * magic) >> (64 - bits);
-}
+size_t get_key(uint64_t occ, uint64_t magic, uint8_t bits) { return (occ * magic) >> (64 - bits); }
 
 /**
  * @brief Gets the minimal value the magic number must be.
@@ -48,9 +47,9 @@ int get_magic_minval(uint64_t occmask, int shift) {
     return exp;
 }
 
-std::pair<uint64_t, bool> find_magic_nbr(int maxiter, int sq, int bits, bool rook,
-                                         std::array<uint64_t, max_size> &occ_bbs,
-                                         std::array<uint64_t, max_size> &atk_bbs) {
+std::optional<std::pair<uint64_t, size_t>> find_magic_nbr(int maxiter, int sq, int bits, bool rook,
+                                                          std::array<uint64_t, max_size> &occ_bbs,
+                                                          std::array<uint64_t, max_size> &atk_bbs) {
     int m = rook ? rook_num_occ_bits[sq] : bishop_num_occ_bits[sq];
     int arrsz = 1 << m;  // size of mask and atk arrays
     int hashsz = 1 << bits;
@@ -60,7 +59,7 @@ std::pair<uint64_t, bool> find_magic_nbr(int maxiter, int sq, int bits, bool roo
     int niter = 0;
     uint64_t occmask = rook ? occupancy_bits_rook(sq) : occupancy_bits_bishop(sq);
     int magic_minval = get_magic_minval(occmask, bits);
-
+    size_t key_maxval = 0;
     while (!found && niter < maxiter) {
         magic = 0;
         niter++;
@@ -73,8 +72,10 @@ std::pair<uint64_t, bool> find_magic_nbr(int maxiter, int sq, int bits, bool roo
             magic = (get_next_magic());
 
         bool is_magic = true;
+        key_maxval = 0;
         for (int n = 0; n < arrsz; n++) {
-            uint64_t key = get_key(occ_bbs[n], magic, bits);
+            size_t key = get_key(occ_bbs[n], magic, bits);
+            key_maxval = std::max(key, key_maxval);
             uint64_t atk_bb = atk_bbs[n];
             if (hash_table[key] == 0xFFFFFFFFFFFFFFFF) {
                 hash_table[key] = atk_bb;
@@ -100,8 +101,12 @@ std::pair<uint64_t, bool> find_magic_nbr(int maxiter, int sq, int bits, bool roo
     // several combinations of the masks n collide on the same hash in the m bit number. We rather
     // mean that if masks n collide, they must also generate the same rook move table. We want
     // collisions if they generate the same table.
-    std::pair<uint64_t, bool> out = {magic, found};
-    return out;
+    if (found) {
+        std::pair<uint64_t, size_t> out = {magic, key_maxval};
+        return out;
+    } else {
+        return {};
+    }
 }
 
 void find_dense(bool rook) {
@@ -117,15 +122,13 @@ void find_dense(bool rook) {
         std::array<uint64_t, max_size> occ_bbs = gen_occ_variation(occ_mask, m);  // will be size
         std::array<uint64_t, max_size> atk_bbs = compute_atk_bbs(occ_bbs, sq, rook);
         int maxiter = 10000000;
-        bool found = false;
-        int outeriter = 50;
-        std::pair<uint64_t, bool> magic_candidate;
+        std::optional<std::pair<uint64_t, size_t>> magic_candidate;
         magic_candidate = find_magic_nbr(maxiter, sq, target_bits, rook, occ_bbs, atk_bbs);
-        found = magic_candidate.second;
-        if (found) {
-            magics[sq] = {magic_candidate.first, target_bits};
+
+        if (magic_candidate) {
+            magics[sq] = {magic_candidate.value().first, target_bits};
             std::cout << "found magic\n " << sq << " " << target_bits << " "
-                      << magic_candidate.first << "\n";
+                      << magic_candidate.value().first << "\n";
         } else {
             std::cout << "did not find magic: " << sq << "\r";
         }
@@ -134,6 +137,52 @@ void find_dense(bool rook) {
     std::cout << "Square   foundbits     magic\n";
     for (int sq = 0; sq < 64; sq++) {
         std::cout << sq << " " << magics[sq].second << " " << magics[sq].first << "\n";
+    }
+}
+/**
+ * @brief For finding magic numbers with the normal amount of bits but a lower hash range than
+ * usual.
+ *
+ * @param[[TODO:direction]] rook [TODO:description]
+ */
+void find_lower_range(bool rook) {
+    std::array<std::pair<uint64_t, size_t>, 64> magics;
+    std::array<std::array<uint64_t, max_size>, 64> occs_bbs_sq;
+    std::array<std::array<uint64_t, max_size>, 64> atk_bbs_sq;
+    for (int sq = 0; sq < 64; sq++) {
+        int target_bits = rook ? rook_num_occ_bits[sq] : bishop_num_occ_bits[sq];
+        uint64_t occ_mask = rook ? rook_occupancy_table[sq] : bishop_occupancy_table[sq];
+        int m = rook ? rook_num_occ_bits[sq] : bishop_num_occ_bits[sq];
+        std::array<uint64_t, max_size> occ_bbs = gen_occ_variation(occ_mask, m);  // will be size
+        std::array<uint64_t, max_size> atk_bbs = compute_atk_bbs(occ_bbs, sq, rook);
+        occs_bbs_sq[sq] = occ_bbs;
+        atk_bbs_sq[sq] = atk_bbs;
+        magics[sq] = {0, 10000000};
+        int maxiter = 100000;
+        int outer = 2000;
+        for (int i = 0; i < outer; i++) {
+            std::optional<std::pair<uint64_t, size_t>> magic_candidate =
+                find_magic_nbr(maxiter, sq, m, rook, occ_bbs, atk_bbs);
+
+            if (magic_candidate) {
+                int upper = magic_candidate.value().second;
+                if (upper < magics[sq].second) {
+                    magics[sq] = magic_candidate.value();
+                    if (upper + 1 != int(1 << target_bits)) {
+                        std::cout << "found magic with better hash range: " << upper + 1
+                                  << " when the standard value is: " << int(1 << target_bits)
+                                  << "\r";
+                    }
+                }
+            }
+            fflush(stdout);
+        }
+    }
+    std::cout << "Square   range     standard         magic\n";
+    for (int sq = 0; sq < 64; sq++) {
+        int target_bits = rook ? rook_num_occ_bits[sq] : bishop_num_occ_bits[sq];
+        std::cout << sq << " " << magics[sq].second + 1 << " " << int(1 << target_bits) << " "
+                  << magics[sq].first << "\n";
     }
 }
 
@@ -150,14 +199,12 @@ void find_sparse(bool rook) {
         occs_bbs_sq[sq] = occ_bbs;
         atk_bbs_sq[sq] = atk_bbs;
         int maxiter = 10000;
-        bool found = false;
 
-        std::pair<uint64_t, bool> magic_candidate;
-        while (!found) {
+        std::optional<std::pair<uint64_t, size_t>> magic_candidate;
+        while (!magic_candidate) {
             magic_candidate = find_magic_nbr(maxiter, sq, target_bits, rook, occ_bbs, atk_bbs);
-            found = magic_candidate.second;
         }
-        magics[sq] = {magic_candidate.first, target_bits};
+        magics[sq] = {magic_candidate.value().first, target_bits};
         std::cout << "found magic: " << sq << "\r";
         fflush(stdout);
     }
@@ -169,6 +216,6 @@ void find_sparse(bool rook) {
 }
 int main() {
 
-    find_sparse(false);
+    find_lower_range(true);
     return 0;
 }
