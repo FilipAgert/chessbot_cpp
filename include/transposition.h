c51b27b6ec7cd65712fe2399d4209c4b2dc60991
Filip Agert <filip.agert@gmail.com>
2025-12-07 15:51:25 +0100

    randomize numbers on startup

--- include/transposition.h
+++ include/transposition.h
@@ -16,14 +16,22 @@ struct transposition_entry {
 
 class ZobroistHasher {
  private:
-    std::array<std::array<uint64_t, 64>, 12> piece_numbers;  // one number for each piece and square
-    std::array<uint64_t, 16> castle_numbers;                 // one for each castle combination
-    std::array<uint64_t, 8> ep_numbers;                      // file of ep
-    uint64_t black_number;                                   // indicate if black is playing or not.
+    static std::array<std::array<uint64_t, 64>, 12>
+        piece_numbers;                               // one number for each piece and square
+    static std::array<uint64_t, 16> castle_numbers;  // one for each castle combination
+    static std::array<uint64_t, 8> ep_numbers;       // file of ep
+    static uint64_t black_number;                    // indicate if black is playing or not.
     static std::uniform_int_distribution<uint64_t> generator;
     static std::mt19937_64 engine;
-    static uint64_t random_uint64_t();
+    /**
+     * @brief Generates a random uint64_t number. Seeded at startup so will be different each
+     * session.
+     *
+     * @return random uint64_t
+     */
+    static uint64_t rand_uint64_t();
     static void initialize_engine();
+    static void randomize_local_vars();
     static const bool s_initialized;
 
  public:
--- src/transposition.cpp
+++ src/transposition.cpp
@@ -5,10 +5,27 @@
 
 const bool ZobroistHasher::s_initialized = []() {
     ZobroistHasher::initialize_engine();
+    ZobroistHasher::randomize_local_vars();
     return true;
 };
 
 void ZobroistHasher::initialize_engine() {
     std::random_device sd;
     ZobroistHasher::engine.seed(sd());
+}
+uint64_t ZobroistHasher::rand_uint64_t() { return generator(engine); }
+void ZobroistHasher::randomize_local_vars() {
+
+    for (int p = 0; p < 12; p++) {
+        for (int sq = 0; sq < 64; sq++) {
+            piece_numbers[p][sq] = ZobroistHasher::rand_uint64_t();
+        }
+    }
+    for (int i = 0; i < 16; i++) {
+        castle_numbers[i] = ZobroistHasher::rand_uint64_t();
+    }
+    for (int i = 0; i < 8; i++) {
+        ep_numbers[i] = ZobroistHasher::rand_uint64_t();
+    }
+    black_number = ZobroistHasher::rand_uint64_t();
 }
