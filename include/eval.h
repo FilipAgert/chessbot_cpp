6037c36198dfa76512d1b241def3f32272e2ecee
Filip Agert <filip.agert@gmail.com>
2025-12-12 21:17:00 +0100

    rename piecevalue struct to a namespace

--- include/eval.h
+++ include/eval.h
@@ -73,28 +73,28 @@ class EvalState {
     }
 };
 
-struct PieceValue {
-    static constexpr int king = 200000;
-    static constexpr int pawn = 100;
-    static constexpr int knight = 290;
-    static constexpr int bishop = 300;
-    static constexpr int rook = 500;
-    static constexpr int queen = 900;
-    static constexpr std::array<int, 7> piecevals = {0, king, queen, rook, knight, bishop, pawn};
+namespace PieceValue {
+static constexpr int king = 200000;
+static constexpr int pawn = 100;
+static constexpr int knight = 290;
+static constexpr int bishop = 300;
+static constexpr int rook = 500;
+static constexpr int queen = 900;
+static constexpr std::array<int, 7> piecevals = {0, king, queen, rook, knight, bishop, pawn};
 
-    static constexpr int inv_frac =
-        10;  // Fraction of extra value piece is worth extra from having more spaces to move to.
-    // Formula is : piece_val * num_moves / max_possible_moves * frac
-    static constexpr int pawn_moveval = 0;
-    static constexpr int knight_moveval = knight / (8 * inv_frac);
-    static constexpr int bishop_moveval = bishop / (14 * inv_frac);
-    static constexpr int rook_moveval = rook / (14 * inv_frac);
-    static constexpr int queen_moveval = 0;
-    static constexpr int king_moveval = -6;
-    static constexpr std::array<int, 7> movevals = {
-        0, king_moveval, queen_moveval, rook_moveval, knight_moveval, bishop_moveval, pawn_moveval};
+static constexpr int inv_frac =
+    10;  // Fraction of extra value piece is worth extra from having more spaces to move to.
+// Formula is : piece_val * num_moves / max_possible_moves * frac
+static constexpr int pawn_moveval = 0;
+static constexpr int knight_moveval = knight / (8 * inv_frac);
+static constexpr int bishop_moveval = bishop / (14 * inv_frac);
+static constexpr int rook_moveval = rook / (14 * inv_frac);
+static constexpr int queen_moveval = 0;
+static constexpr int king_moveval = -6;
+static constexpr std::array<int, 7> movevals = {
+    0, king_moveval, queen_moveval, rook_moveval, knight_moveval, bishop_moveval, pawn_moveval};
 
-    static constexpr int king_dist2centre_value = 20;  // 20 per distance.
-};
+static constexpr int king_dist2centre_value = 20;  // 20 per distance.
+};  // namespace PieceValue
 
 #endif
