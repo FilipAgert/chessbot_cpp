8de0f8ac615009b293937b8b89bc68ba26d109a7
Filip Agert <filip.agert@gmail.com>
2025-12-14 15:48:49 +0100

    template in header

--- include/moveorder.h
+++ include/moveorder.h
@@ -1,9 +1,11 @@
 #ifndef MOVEORDER_H
 #define MOVEORDER_H
 
+#include "eval.h"
 #include <board.h>
 #include <move.h>
 #include <optional>
+#include <utility>
 namespace MoveOrder {
 void partial_move_sort(std::array<Move, max_legal_moves> &moves,
                        std::array<int, max_legal_moves> &scores, size_t start, size_t num_moves,
@@ -30,7 +32,14 @@ inline void partial_move_sort(std::array<Move, max_legal_moves> &moves,
  * @param[[TODO:direction]] board [TODO:description]
  */
 template <bool is_white>
-void apply_move_sort(std::array<Move, max_legal_moves> &moves, size_t num_moves, Board &board);
+void apply_move_sort(std::array<Move, max_legal_moves> &moves, size_t num_moves, Board &board) {
+    std::array<int, max_legal_moves> move_scores;
+    for (int m = 0; m < num_moves; m++) {
+        int move_score = move_heuristics<is_white>(moves[m], board);
+        move_scores[m] = move_score;
+    }
+    partial_move_sort<is_white>(moves, move_scores, num_moves, false);
+}
 /**
  * @brief Sort moves, and set the firstmove first no matter the other metrics.
  *
@@ -41,7 +50,28 @@ void apply_move_sort(std::array<Move, max_legal_moves> &moves, size_t num_moves,
  */
 template <bool is_white>
 void apply_move_sort(std::array<Move, max_legal_moves> &moves, size_t num_moves,
-                     std::optional<Move> firstmove, Board &board);
+                     std::optional<Move> firstmove, Board &board) {
+    if (firstmove) {
+        std::array<int, max_legal_moves> move_scores;
+        int firstmoveidx = -1;
+        Move first = firstmove.value();
+        for (int m = 0; m < num_moves; m++) {
+            int move_score = move_heuristics<is_white>(moves[m], board);
+            if (moves[m].source == first.source && moves[m].target == first.target)
+                firstmoveidx = m;
+            move_scores[m] = move_score;
+        }
+        if (firstmoveidx == -1) {
+            std::cout << firstmove.value().toString() << std::endl;
+            throw std::runtime_error("firstmove was not found in movelist");
+        }
+        std::swap(move_scores[0], move_scores[firstmoveidx]);
+        std::swap(moves[0], moves[firstmoveidx]);
+        partial_move_sort<is_white>(moves, move_scores, 1, num_moves, false);
+    } else {
+        apply_move_sort<is_white>(moves, num_moves, board);
+    }
+}
 
 /**
  * @brief Rate move by Heuristics
@@ -50,6 +80,25 @@ void apply_move_sort(std::array<Move, max_legal_moves> &moves, size_t num_moves,
  * @param[in] board board
  * @return score of move to sort by
  */
-template <bool is_white> int move_heuristics(Move &move, Board &board);
+template <bool is_white> int move_heuristics(Move &move, Board &board) {
+    int heuristics = 0;
+    if (!board.is_square_empty(move.target)) {
+        heuristics += PieceValue::piecevals[board.get_piece_at(move.target).get_type()] * 10 -
+                      PieceValue::piecevals[board.get_piece_at(move.source).get_type()];
+    }
+
+    if (move.promotion.get_type() > 0) {
+        heuristics += PieceValue::piecevals[move.promotion.get_type()];
+    }
+
+    BB pawn_atk_bb = movegen::pawn_atk_bb<is_white>(board.get_piece_bb<pieces::pawn, is_white>());
+    if ((BitBoard::one_high(move.target) & pawn_atk_bb) > 0) {
+        heuristics -= PieceValue::piecevals[board.get_piece_at(move.source)
+                                                .get_type()];  // penalize if target square is on a
+                                                               // square defended by a pawn.
+    }
+
+    return heuristics;
+}
 };  // namespace MoveOrder
 #endif
--- src/moveorder.cpp
+++ src/moveorder.cpp
@@ -1,7 +1,5 @@
 // Copyright 2025 Filip Agert
 #include <algorithm>
-#include <eval.h>
-#include <integer_representation.h>
 #include <moveorder.h>
 #include <vector>
 void MoveOrder::partial_move_sort(std::array<Move, max_legal_moves> &moves,
@@ -24,59 +22,4 @@ void MoveOrder::partial_move_sort(std::array<Move, max_legal_moves> &moves,
 
     std::copy(sorted_moves.begin() + start, sorted_moves.begin() + num_moves,
               moves.begin() + start);
-}
-template <bool is_white> int MoveOrder::move_heuristics(Move &move, Board &board) {
-    int heuristics = 0;
-    if (!board.is_square_empty(move.target)) {
-        heuristics += PieceValue::piecevals[board.get_piece_at(move.target).get_type()] * 10 -
-                      PieceValue::piecevals[board.get_piece_at(move.source).get_type()];
-    }
-
-    if (move.promotion.get_type() > 0) {
-        heuristics += PieceValue::piecevals[move.promotion.get_type()];
-    }
-
-    BB pawn_atk_bb = movegen::pawn_atk_bb<is_white>(board.get_piece_bb<pieces::pawn, is_white>());
-    if ((BitBoard::one_high(move.target) & pawn_atk_bb) > 0) {
-        heuristics -= PieceValue::piecevals[board.get_piece_at(move.source)
-                                                .get_type()];  // penalize if target square is on a
-                                                               // square defended by a pawn.
-    }
-
-    return heuristics;
-}
-
-template <bool is_white>
-void MoveOrder::apply_move_sort(std::array<Move, max_legal_moves> &moves, size_t num_moves,
-                                Board &board) {
-    std::array<int, max_legal_moves> move_scores;
-    for (int m = 0; m < num_moves; m++) {
-        int move_score = move_heuristics<is_white>(moves[m], board);
-        move_scores[m] = move_score;
-    }
-    partial_move_sort<is_white>(moves, move_scores, num_moves, false);
-}
-template <bool is_white>
-void MoveOrder::apply_move_sort(std::array<Move, max_legal_moves> &moves, size_t num_moves,
-                                std::optional<Move> firstmove, Board &board) {
-    if (firstmove) {
-        std::array<int, max_legal_moves> move_scores;
-        int firstmoveidx = -1;
-        Move first = firstmove.value();
-        for (int m = 0; m < num_moves; m++) {
-            int move_score = move_heuristics<is_white>(moves[m], board);
-            if (moves[m].source == first.source && moves[m].target == first.target)
-                firstmoveidx = m;
-            move_scores[m] = move_score;
-        }
-        if (firstmoveidx == -1) {
-            std::cout << firstmove.value().toString() << std::endl;
-            throw std::runtime_error("firstmove was not found in movelist");
-        }
-        std::swap(move_scores[0], move_scores[firstmoveidx]);
-        std::swap(moves[0], moves[firstmoveidx]);
-        partial_move_sort<is_white>(moves, move_scores, 1, num_moves, false);
-    } else {
-        apply_move_sort<is_white>(moves, num_moves, board);
-    }
 }
